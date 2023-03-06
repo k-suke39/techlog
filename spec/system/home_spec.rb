42f2d77edbca2ea769c0af796474daaf90127e11
KeisukeSakuma <k.sakuma@ordermade.jp>
2023-03-06 16:28:54 +0900

    ✅ add : トップページの統合テストを追加

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Homes', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  pending "add some scenarios (or delete) #{__FILE__}"
+
+  describe 'validates of toppage'do
+    it 'diplayed with HOME#top' do
+      visit '/'
+      expect(page).to have_content('Home#top')
+    end
+  end
+end
