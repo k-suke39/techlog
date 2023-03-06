ae53b4efe09e3f9e3e9707d1e66e99b45e162966
KeisukeSakuma <k.sakuma@ordermade.jp>
2023-03-06 16:29:46 +0900

    ⚡️ mod: Headlessモードでのテスト修正

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Homes', type: :system do
   before do
-    driven_by(:rack_test)
+    driven_by :selenium_chrome_headless
   end
 
   pending "add some scenarios (or delete) #{__FILE__}"
